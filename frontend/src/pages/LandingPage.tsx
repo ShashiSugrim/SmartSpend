d4d6c3dcd40bb8b11893ee2ac2d9daf50e11c906
kyoshinoda <kyoshisew@gmail.com>
2025-10-18 14:39:41 -0400

    Added Features

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/Landing/Hero.tsx";
+import Feature from "@/components/Landing/Feature.tsx";
 
 const LandingPage = () => {
     return(
         <div className= "h-screen w-screen">
             <Hero/>
+            <Feature/>
         </div>
     );
 }
